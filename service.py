b8ab25d19e58454acd4c2d9327aa81d182834872
omytty <omytty@163.com>
2018-10-21 21:45:17 +0800

    update

---
+++ service.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import json
+import asyncio
+import websockets
+
+connected = set()
+
+async def hello(websocket, path):
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            await asyncio.wait([user.send(message) for user in connected])
+    finally:
+        connected.remove(websocket)
+
+if __name__ == '__main__':
+    start_server = websockets.serve(hello, 'localhost', 8765)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_server)
+    loop.run_forever()
